bd2dbe5694d0299c1d31abd8d6dd240f757aeb66
unknown <alejandropuerto0504@gmail.com>
2025-10-07 20:27:11 -0500

    Se añade base de datos en tipo de archivo plano

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function ($) {
     "use strict";
-    
+
     // Dropdown on mouse hover
     $(document).ready(function () {
         function toggleNavbarMethod() {
@@ -17,8 +17,8 @@
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -28,10 +28,10 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
-    
+
 
     // Date and time picker
     $('.date').datetimepicker({
@@ -51,20 +51,48 @@
         loop: true,
         center: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:1
+            576: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             }
         }
     });
-    
+
+
+    //Datos de archios planos
+    fetch('../db/productos.txt')
+        .then(res => res.text())
+        .then(data => {
+            const contenedor = document.getElementById('producto');
+            const lineas = data.trim().split('\n');
+
+            lineas.forEach(linea => {
+                const [imagen, nombre, precio, descripcion] = linea.split(',');
+                const productoHTML = `
+          <div class="col-lg-6 mb-5">
+            <div class="row align-items-center">
+              <div class="col-sm-5">
+                <img class="img-fluid mb-3 mb-sm-0" src="${imagen.trim()}" alt="">
+              </div>
+              <div class="col-sm-7">
+                <h4><i class="fa fa-dollar-sign service-icon"></i>${precio.trim()}</h4>
+                <h3>${nombre.trim()}</h3>
+                <p class="m-0">${descripcion.trim()}</p>
+              </div>
+            </div>
+          </div>
+        `;
+                contenedor.innerHTML += productoHTML;
+            });
+        })
+        .catch(err => console.error('Error cargando productos:', err));
 })(jQuery);
 
